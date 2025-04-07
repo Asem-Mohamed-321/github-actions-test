391633852039149b32e2337140ef44d8faf68813
Asem Mohamed Ali <167926594+Asem-Mohamed-321@users.noreply.github.com>
2025-04-07 13:43:53 +0200

    Update lab.js

--- lab.js
+++ lab.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send(`<h2>hello world</h2><h3>This is the docker lab</h3>`)
+    res.send(`<h2>hello world</h2><h3>This is the docker lab <b>after edinting</b></h3>`)
 })
 
 app.listen(8085);
